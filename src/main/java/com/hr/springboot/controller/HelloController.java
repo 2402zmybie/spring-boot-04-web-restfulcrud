96898278c5d8f5094e422f44d3943e86e5791f1e
2402zmybie <2402368990@qq.com>
2020-08-04 12:08:42 +0800

    thymeleaf显示数据

--- src/main/java/com/hr/springboot/controller/HelloController.java
+++ src/main/java/com/hr/springboot/controller/HelloController.java
@@ -1,13 +1,28 @@
 package com.hr.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "hello world";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map) {
+        //classpath:/templates/success.html
+        //给页面设置值, 用于thymeleaf模板引擎解析
+        map.put("hello","<h1>你好呀</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
     }
 }
